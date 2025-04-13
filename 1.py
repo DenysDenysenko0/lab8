4d5da3340f013b51627a899d74c26e1bb76a42f0
AlexKubusha <kubusha.oleksii@student.sumdu.edu.ua>
2025-04-13 20:07:05 +0300

    Додано функцію  Кубуши Олексія (сортування оцінки по вищій математиці)

--- 1.py
+++ 1.py
@@ -10,9 +10,10 @@ def print_teams(students):
         s2 = data[3]
         s3 = data[4]
         s4 = data[5]
-        print(f"{pib} - {grypa}, {kyrs} курс. Вища математика: {s1}, Дискретна математика: {s2}, Програмування: {s3}, Програмування мовою пайтон: {s4}")
-    
+        print(
+            f"{pib} - {grypa}, {kyrs} курс. Вища математика: {s1}, Дискретна математика: {s2}, Програмування: {s3}, Програмування мовою пайтон: {s4}")
     MenuQuestion()
+
 
 # Функція Денисенка Дениса для додавання даних у словник
 def add_team(students, pib):
@@ -23,7 +24,7 @@ def add_team(students, pib):
             print("Операцію скасовано.")
             MenuQuestion()
             return
-    
+
     kyrs = input("Введіть курс студента: ")
     grypa = input("Введіть групу студента: ")
     s1 = input("Введіть оцінку за вищу математику: ")
@@ -36,6 +37,22 @@ def add_team(students, pib):
     print("Студента додано/оновлено.")
     MenuQuestion()
 
+
+# Функція Кубуші Олексія сортування студентів за оцінкою з вищої математики (від найбільшої до найменшої)
+def filter_by_math_grade(students):
+    print("\nСортування студентів за оцінкою з вищої математики (від найбільшої до найменшої):")
+    sorted_students = sorted(
+        students.items(),
+        key=lambda item: float(item[1][2]),  # оцінка з вищої математики
+        reverse=True
+    )
+
+    for pib, data in sorted_students:
+        print(f"{pib} - Вища математика: {data[2]}")
+    MenuQuestion()
+
+
+# Повернення в меню
 def MenuQuestion():
     answer = input("\nВийти у меню?(Так/ні, завершення роботи): ")
     if answer.lower() == "так":
@@ -43,10 +60,13 @@ def MenuQuestion():
     else:
         print("Завершення роботи.")
 
+
+# Меню
 def Menu():
     print("\nМеню:")
     print("1 - Показати журнал")
     print("2 - Додати студента")
+    print("3 - Сортувати за оцінкою з вищої математики")
     print("0 - Завершення роботи")
 
     num = input("Оберіть пункт меню: ")
@@ -56,10 +76,13 @@ def Menu():
     elif num == "2":
         pib = input("Введіть ПІБ студента (Прізвище_ім'я_побатькові): ")
         add_team(students, pib)
+    elif num == "3":
+        filter_by_math_grade(students)
     elif num == "0":
         print("Завершення роботи.")
     else:
         print("\nНевірний вибір!\n")
         Menu()
+
 
 Menu()
